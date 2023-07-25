cc216ee629054d8aa7a130dc307a0cac7f3da83f
Joao Vitor <joaovlopes000@gmail.com>
2023-07-24 21:36:45 -0300

    finished controllers overview

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, Param, Post, Put, Query, Redirect, Req } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
   @Header('Cache-Control', 'none')
   @HttpCode(204)
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
@@ -32,5 +33,15 @@ export class CatsController {
     if (version && version === '5') {
       return { url: 'https://docs.nestjs.com/v5/' };
     }
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: CreateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
